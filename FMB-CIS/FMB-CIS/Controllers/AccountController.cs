f427358377b55c90bb018c79788fc7a08886020a
mybusybeedale <dale@mybusybee.net>
2023-09-06 15:52:05 +0800

    Developed login module

---
+++ FMB-CIS/FMB-CIS/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using FMB_CIS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMB_CIS.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPasswordSent()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
