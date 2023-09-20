9e8f8d06403c63cd96c4155f99d8d85ecb2d72d5
jask01 <jask@mybusybee.net>
2023-09-20 09:16:42 +0800

    added importer dashboard

--- FMB-CIS/FMB-CIS/Controllers/DashboardController.cs
+++ FMB-CIS/FMB-CIS/Controllers/DashboardController.cs
@@ -38,7 +38,7 @@ namespace FMB_CIS.Controllers
             ViewModel mymodel = new ViewModel();
             int userID = Convert.ToInt32(((ClaimsIdentity)User.Identity).FindFirst("userID").Value);
             string userRole = ((ClaimsIdentity)User.Identity).FindFirst("userRole").Value;
-             if (((ClaimsIdentity)User.Identity).FindFirst("userID").Value != null)
+            if (((ClaimsIdentity)User.Identity).FindFirst("userID").Value != null)
             {    
                 /*DataTable dtbl = new DataTable();
                 using (SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("ConnStrng")))
@@ -51,26 +51,26 @@ namespace FMB_CIS.Controllers
                     sqlDa.Fill(dtbl);
                 }*/
                 
-            var ChainsawList = _context.tbl_chainsaw.ToList();
-            var ChainsawOwnedList = ChainsawList.Where(m => m.user_id == userID && m.status == userRole).ToList();
+                var ChainsawList = _context.tbl_chainsaw.ToList();
+                var ChainsawOwnedList = ChainsawList.Where(m => m.user_id == userID && m.status == "Seller").ToList();
 
-            var applicationlist = from a in _context.tbl_application
-                                  where a.tbl_user_id == userID
-                                  select a;
+                var applicationlist = from a in _context.tbl_application
+                                      where a.tbl_user_id == userID
+                                      select a;
 
 
-            var applicationtypelist = _context.tbl_application_type;
+                var applicationtypelist = _context.tbl_application_type;
 
-            var applicationMod = from a in applicationlist
-                                 join appt in applicationtypelist on a.tbl_application_type_id equals appt.id
-                                 join pT in _context.tbl_permit_type on a.tbl_application_type_id equals pT.id
-                                 join pS in _context.tbl_permit_status on appt.id equals pS.application_type
-                                 where a.tbl_user_id == userID
-                                 select new ApplicationModel{id = a.id, application_type = appt.name, permit_type = pT.name, permit_status = pS.status};
-            mymodel.tbl_Chainsaws = ChainsawOwnedList;
-            mymodel.applicationModels = applicationMod;
+                var applicationMod = from a in applicationlist
+                                     join appt in applicationtypelist on a.tbl_application_type_id equals appt.id
+                                     join pT in _context.tbl_permit_type on a.tbl_application_type_id equals pT.id
+                                     join pS in _context.tbl_permit_status on appt.id equals pS.application_type
+                                     where a.tbl_user_id == userID
+                                     select new ApplicationModel{id = a.id, application_type = appt.name, permit_type = pT.name, permit_status = pS.status};
+                mymodel.tbl_Chainsaws = ChainsawOwnedList;
+                mymodel.applicationModels = applicationMod;
 
-            return View(mymodel);
+                return View(mymodel);
 
             }
             return View();
