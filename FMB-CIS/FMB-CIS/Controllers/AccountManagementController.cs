6705fc50348b5dfc8843eb4ce0dd81489f1ab277
mybusybeedale <dale@mybusybee.net>
2023-09-22 14:54:03 +0800

    Added under construction page

---
+++ FMB-CIS/FMB-CIS/Controllers/AccountManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FMB_CIS.Controllers
+{
+    public class AccountManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FMB-CIS/FMB-CIS/Controllers/CreationOfNewDenrOfficeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FMB_CIS.Controllers
+{
+    public class CreationOfNewDenrOfficeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
