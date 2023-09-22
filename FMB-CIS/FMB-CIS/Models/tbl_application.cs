52f4fc3e75432ca052ec3da763f9c3c9e7751670
Mugen <louis@mybusybee.net>
2023-09-22 17:13:20 +0800

    Added model, controller for chainsaw seller

--- FMB-CIS/FMB-CIS/Models/tbl_application.cs
+++ FMB-CIS/FMB-CIS/Models/tbl_application.cs
@@ -1,16 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FMB_CIS.Models
 {
     public class tbl_application
     {
         public int id { get; set; }
-
         public int tbl_user_id { get; set; }
         public int tbl_application_type_id { get; set; }
         public int tbl_permit_type_id { get; set; }
-        //public string? Status { get; set; }
-        //public ICollection<tbl_application_type> application_type { get; set; }
-
-}
+        public string supplier_fname { get; set; }
+        public string supplier_mname { get; set; }
+        public string supplier_lname { get; set; }
+        public string supplier_suffix { get; set; }
+        public string supplier_address { get; set; }
+        public string supplier_email {  get; set; }
+        public string supplier_contact_no { get; set; }
+        public string qty { get; set; }
+        public string origin { get; set; }
+        public string purpose { get; set; }
+        public string expected_time_arrival { get; set; }
+        public string expected_time_release { get; set; }
+        public string date_of_inspection { get; set; }
+        public int status { get; set; }
+    }
 }
