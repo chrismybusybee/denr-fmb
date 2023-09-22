8801d37a4d1588ba56f69e4067ed38211ab4e089
Mugen <louis@mybusybee.net>
2023-09-22 17:13:20 +0800

    Added model, controller for chainsaw seller

--- FMB-CIS/FMB-CIS/Controllers/ChainsawSellerController.cs
+++ FMB-CIS/FMB-CIS/Controllers/ChainsawSellerController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using FMB_CIS.Data;
+using FMB_CIS.Models;
 
 namespace FMB_CIS.Controllers
 {
     public class ChainsawSellerController : Controller
     {
+        private readonly LocalContext _context;
+        private readonly IConfiguration _configuration;
+
+        public ChainsawSellerController(IConfiguration configuration, LocalContext context)
+        {
+            this._configuration = configuration;
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(tbl_application sm)
+        {
+            sm.tbl_application_type_id = 3;
+            sm.tbl_user_id = Convert.ToInt32(((ClaimsIdentity)User.Identity).FindFirst("userID").Value);
+            sm.status = 1;
+            sm.tbl_permit_type_id = 3;
+
+            _context.tbl_application.Add(sm);
+
+            ViewBag.Status = "Save Success.";
+
+            return View("Index");
         }
     }
 }
