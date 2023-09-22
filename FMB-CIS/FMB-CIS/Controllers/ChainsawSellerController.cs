08d4ddb0bcc4db1c305596d4435cbc9f1321931c
mybusybeedale <dale@mybusybee.net>
2023-09-22 12:38:41 +0800

    Added chainsaw seller application

---
+++ FMB-CIS/FMB-CIS/Controllers/ChainsawSellerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FMB_CIS.Controllers
+{
+    public class ChainsawSellerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
