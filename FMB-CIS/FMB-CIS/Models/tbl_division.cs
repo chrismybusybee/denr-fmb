362fc3cbb4a8d0027f8ad9db2c429ee0742ab002
Jayson Castro <arvin@mybusybee.net>
2023-11-08 09:25:11 +0800

    Add new Model Entities tbl_access_right, tbl_office_type, tbl_siste_settings, tbl_user_type_access_right

--- FMB-CIS/FMB-CIS/Models/tbl_division.cs
+++ FMB-CIS/FMB-CIS/Models/tbl_division.cs
@@ -4,7 +4,7 @@
     {
         public int? id { get; set; }
         public string? office_name { get; set; }
-        public string? department { get; set; }
+        public string? department { get; set; } // Linked to tbl_office_type
         public int? region_id { get; set; }
         public int? province_id { get; set; }
         public string? company_name { get; set; }
---
+++ FMB-CIS/FMB-CIS/Models/tbl_site_settings.cs
@@ -0,0 +1,11 @@
+﻿namespace FMB_CIS.Models
+{
+    public class tbl_site_settings
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string value { get; set; } // true, false, list, regex... allows system flexibility / maintainability, minimizes code edit
+        public string scope { get; set; } // Page, Module, Functionality
+    }
+}
