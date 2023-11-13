42ad312897875dd2781aaf4b634ad510a9c9bd9d
Jayson Castro <arvin@mybusybee.net>
2023-11-13 15:11:31 +0800

    Add Class Library for Utilities Add Dashboard Checking

---
+++ FMB-CIS/Services/Utilities/AccessRights.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.Utilities
+{
+    public static class AccessRightsUtilities
+    {
+        public static bool IsAccessRights(string accessRights, string accessRight)
+        {
+            var accessRightsList = accessRights.Split(",");
+            return accessRightsList.FirstOrDefault(a => a == accessRight) != null;
+        }
+    }
+}
