e0cbf05052109d02498b9a5e711ac675809c32af
arvin@mybusybee.net <arvinmybusybee>
2023-12-27 15:59:36 +0800

    Document Tagging Checklist

--- FMB-CIS/FMB-CIS/Models/tbl_files_checklist_bridge.cs
+++ FMB-CIS/FMB-CIS/Models/tbl_files_checklist_bridge.cs
@@ -5,5 +5,6 @@
         public int id {  get; set; }
         public int? tbl_files_id { get; set; }
         public int? tbl_document_checklist_id { get; set; }
+        public string? status { get; set; }
     }
 }
