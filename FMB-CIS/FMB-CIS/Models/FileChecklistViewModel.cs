75a245990b8842062e903f465f1b370e92765b8d
jask01 <jask@mybusybee.net>
2023-12-28 18:08:58 +0800

    Multiple File Tagging

--- FMB-CIS/FMB-CIS/Models/FileChecklistViewModel.cs
+++ FMB-CIS/FMB-CIS/Models/FileChecklistViewModel.cs
@@ -5,5 +5,8 @@
         public string? FileName { get; set; }
         public int? tbl_document_checklist_id { get; set; }
         public string? tbl_document_checklist_name { get; set; }
+
+        //used for selectize
+        public List<string>? FileNames { get; set; }
     }
 }
