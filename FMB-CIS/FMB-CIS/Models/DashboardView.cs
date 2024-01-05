ed9e0cd53fa89a66c1c18a8d8fd5e85b125a5055
Jayson Castro <arvin@mybusybee.net>
2024-01-05 17:58:42 +0800

    Dashboard, Mobile, Views

---
+++ FMB-CIS/FMB-CIS/Models/DashboardView.cs
@@ -0,0 +1,28 @@
+﻿namespace FMB_CIS.Models
+{
+    public class DashboardView
+    {
+        public int UserActiveCount { get; set; } = 0;
+        public int UserChangeRequestCount { get; set; } = 0;
+        public int OfficeTypesCount { get; set; } = 0;
+        public int OfficesCount { get; set; } = 0;
+        public int UserTypesCount { get; set; } = 0;
+        public int AccessRightsCount { get; set; } = 0;
+        public int WorkflowsCount { get; set; } = 0;
+        public int AnnouncementsCount { get; set; } = 0;
+        public int EmailsCount { get; set; } = 0;
+        public int ChecklistsCount { get; set; } = 0;
+        public int SellerPendingCount { get; set; } = 0;
+        public int SellerApprovedCount { get; set; } = 0;
+        public int SellerDeclinedCount { get; set; } = 0;
+        public int SellerTotalCount { get; set; } = 0;
+        public int OwnerPendingCount { get; set; } = 0;
+        public int OwnerApprovedCount { get; set; } = 0;
+        public int OwnerDeclinedCount { get; set; } = 0;
+        public int OwnerTotalCount { get; set; } = 0;
+        public int ImporterPendingCount { get; set; } = 0;
+        public int ImporterApprovedCount { get; set; } = 0;
+        public int ImporterDeclinedCount { get; set; } = 0;
+        public int ImporterTotalCount { get; set; } = 0;
+    }
+    }
