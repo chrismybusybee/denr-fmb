d77fa97f0532d9c87e788a2589e326a3ec09cf72
jask01 <jask@mybusybee.net>
2024-01-17 09:49:11 +0800

    Report Generation Side Menu and Index Page

---
+++ FMB-CIS/FMB-CIS/Controllers/ReportGenerationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FMB_CIS.Controllers
+{
+    public class ReportGenerationController : Controller
+    {
+        [RequiresAccess(allowedAccessRights = "allow_page_report_generation")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
