dfd3482b4237d0fe71e64f71a07f006d13a1a73a
jask01 <jask@mybusybee.net>
2024-02-05 14:41:29 +0800

    Permit Status progress bar on CoR (applicant's view)

--- FMB-CIS/FMB-CIS/Controllers/ApplicationController.cs
+++ FMB-CIS/FMB-CIS/Controllers/ApplicationController.cs
@@ -175,12 +175,13 @@ namespace FMB_CIS.Controllers
             //HISTORY
             var applicationtypelist = _context.tbl_application_type;
 
-            var applicationMod = from a in applicationlist
+            var applicationMod = (from a in applicationlist
                                  join usr in _context.tbl_user on a.tbl_user_id equals usr.id
                                  join appt in applicationtypelist on a.tbl_application_type_id equals appt.id
                                  join pT in _context.tbl_permit_type on a.tbl_permit_type_id equals pT.id
                                  join pS in _context.tbl_permit_status on a.status equals pS.id
-                                 join pSs in _context.tbl_permit_statuses on a.status equals pSs.id
+                                 //join pSs in _context.tbl_permit_statuses on a.status equals pSs.id
+                                 join wfs in _context.tbl_permit_workflow_step on new { permitType = pT.id.ToString(), status = a.status.ToString() } equals new { permitType = wfs.permit_type_code, status = wfs.workflow_step_code }
                                  where pT.name == "Certificate of Registration"
                                  select new ApplicantListViewModel 
                                  { 
@@ -192,13 +193,21 @@ namespace FMB_CIS.Controllers
                                      address = usr.street_address,
                                      application_type = appt.name, 
                                      permit_type = pT.name,
-                                     permit_status = pSs.status,
-                                     permit_status_id = pSs.id,
+                                     permit_status = pS.status,
+                                     permit_status_id = pS.id,
+                                     permit_statuses = wfs.name,
                                      tbl_user_id = (int)usr.id,
                                      date_of_expiration = a.date_of_expiration,
                                      qty = a.qty,
-                                     ReferenceNo = a.ReferenceNo
-                                 };
+                                     ReferenceNo = a.ReferenceNo,
+                                     currentStepCount = (int)Math.Ceiling((decimal)a.status / 2), // Soon be dynamic
+                                     currentMaxCount = usr.tbl_region_id == 13 ? 6 : 10,// Soon be dynamic 
+                                 }).ToList();
+
+            foreach (ApplicantListViewModel mod in applicationMod)
+            {
+                mod.currentPercentage = (mod.currentStepCount * 100 / mod.currentMaxCount);
+            }
 
             mymodel.applicantListViewModels = applicationMod;
 
